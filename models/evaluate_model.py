60fedad9c6e101f0359fc53145ba61bb5107f407
Sai Sujith MarapaReddy <sai.marapareddy@gmail.com>
2017-10-15 13:55:58 -0700

    Added method to evaluate_model
    Multiclass (10) text classification solved through
    bi-directional LSTM (recurrent neural networks).
    
    Added method to evaluate_model
    
    Author:  Sai MarapaReddy <sai.marapareddy@gmail.com>
    Committer: Sai MarapaReddy <sai.marapareddy@gmail.com>

--- models/evaluate_model.py
+++ models/evaluate_model.py
@@ -1,12 +1,13 @@
-from build_model import *
+from models.build_model import *
 
-print('Evaluating the model ...')
-model.fit(train_data_1, Y_train,
-          batch_size=64,
-          epochs=1,
-          validation_data=(test_data_1, Y_test),class_weight=class_weight)
-score, acc = model.evaluate(test_data_1, Y_test,
-                            batch_size=64)
-print('Test score:', score)
-print('Test accuracy:', acc)
 
+def evaluate(model):
+    print('Evaluating the model ...')
+    model.fit(train_data_1, Y_train,
+              batch_size=64,
+              epochs=1,
+              validation_data=(test_data_1, Y_test),class_weight=class_weight)
+    score, acc = model.evaluate(test_data_1, Y_test,
+                                batch_size=64)
+    print('Test score:', score)
+    print('Test accuracy:', acc)
